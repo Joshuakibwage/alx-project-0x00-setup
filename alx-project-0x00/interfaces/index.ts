5b32aab4f6de721d7b25ebb9b36ae1a23cd04ee5
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-19 18:25:31 +0300

    feat: add button component

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,11 @@
 export interface PillProps {
   title: string
 }
+
+// interfaces/index.tsx
+export interface ButtonProps {
+  title: string;
+  size?: 'small' | 'medium' | 'large';
+  rounded?: 'rounded-sm' | 'rounded-md' | 'rounded-full';
+  className?: string; // Optional for extra styles
+}
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,25 @@
-import Card from "@/components/Card"
+import Card from "@/components/Card";
+import Button from "@/components/Button";
+
 
 const Landing: React.FC = () => {
     return (
         <div>
             <h1 className="text-xl font-extralight ">Landing Page</h1>
             <Card />
+
+            <Button title="Small / rounded-sm" size="small" rounded="rounded-sm" />
+            <Button title="Small / rounded-md" size="small" rounded="rounded-md" />
+            <Button title="Small / rounded-full" size="small" rounded="rounded-full" />
+
+            <Button title="Medium / rounded-sm" size="medium" rounded="rounded-sm" />
+            <Button title="Medium / rounded-md" size="medium" rounded="rounded-md" />
+            <Button title="Medium / rounded-full" size="medium" rounded="rounded-full" />
+
+            <Button title="Large / rounded-sm" size="large" rounded="rounded-sm" />
+            <Button title="Large / rounded-md" size="large" rounded="rounded-md" />
+            <Button title="Large / rounded-full" size="large" rounded="rounded-full" />
+            
         </div>
     )
 }
