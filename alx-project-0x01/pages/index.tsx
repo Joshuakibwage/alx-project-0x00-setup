c73cdda3fc1933a765f20f37b10437831c5539b5
Joshua Kibwage <joshuakibwage23@gmail.com>
2025-07-20 14:53:26 +0300

    chore: scaffold nextjs project

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -2,7 +2,7 @@
 
 const Home: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen ">
+    <div className="flex justify-center items-center h-screen w-full">
       <h1 className="text-7xl font-thin">Welcome Page</h1>
     </div>
   )
